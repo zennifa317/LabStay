8c3051218ae4c0d2b864acebeb6a7b183b02eb59
zenifa317 <zenifa2001317@gmail.com>
2025-07-22 01:17:21 +0900

    feat: 画像分類の検出機能を実装
    - detect.pyに画像を入力として受け取り、モデルを使用して予測を行うdetect関数を追加
    - 画像の前処理を行うためのtransformを定義
    - モデルの重みを読み込み、指定された画像に対して予測を実行するメイン処理を追加

---
+++ ImageClassification/detect.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import torch
+from torchvision.transforms import v2
+
+from model import LabStayModel
+
+def detect(image_path, model, device):
+    model.eval()
+
+    detect_transform = v2.Compose([
+        v2.ToImage(),
+        v2.Resize((224, 224)),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    with torch.no_grad():
+        image = Image.open(image_path).convert('RGB')
+        image = detect_transform(image)
+        image = image.unsqueeze(0).to(device)
+        
+        pred = model(image)
+        pred = pred.item()
+
+    return pred
+
+if __name__ == '__main__':
+    device = 'cuda'
+    weight_path = 'path/to/weights.pth'
+    image_path = 'path/to/image.jpg'
+    
+    model = LabStayModel()
+    model.to(device)
+
+    model.load_state_dict(torch.load(weight_path))
+
+    pred = detect(image_path, model, device)
+    pred.argmax(dim=1) #治す
+    print(f"Predicted class: {pred}")
