671f247770783a43880c87aedd28ad066ecf6084
zenifa317 <zenifa2001317@gmail.com>
2025-07-22 18:29:19 +0900

    feat: LabStayModelの初期化処理を実装
    - モデルの初期化メソッドに特徴抽出層と分類層を追加しました。
    - 特徴抽出層には3つの畳み込み層とReLU活性化関数、プーリング層を含めました。
    - 分類層にはフラット化層と2つの全結合層を追加し、クラス数を引数として受け取るようにしました。

--- ImageClassification/model.py
+++ ImageClassification/model.py
@@ -1,9 +1,27 @@
 import torch.nn as nn
 
 class LabStayModel(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=5):
         super().__init__()
-        pass
-    
+        self.feature = nn.Sequential(
+            nn.Conv2d(3, 3, kernel_size=2, stride=2, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(3, 6, kernel_size=2, stride=2, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 6, kernel_size=2, stride=2, padding=0),
+            nn.ReLU()
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(6 * 7 * 7, 20),
+            nn.ReLU(),
+            nn.Linear(20, num_classes)
+        )
+
     def forward(self, x):
-        pass
+        x = self.feature(x)
+        x = self.classifier(x)
+        
+        return x
