924d894437021b132e510fbc65be7479ff19b196
zenifa317 <zenifa2001317@gmail.com>
2025-07-22 19:50:04 +0900

    feat: LabStayModelの特徴抽出と分類器の実装を削除
    LabStayModelクラスの初期化処理から特徴抽出層と分類器の実装を削除しました。これにより、モデルの構造を簡素化しました。

--- ImageClassification/model.py
+++ ImageClassification/model.py
@@ -3,25 +3,7 @@ import torch.nn as nn
 class LabStayModel(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        self.feature = nn.Sequential(
-            nn.Conv2d(3, 3, kernel_size=2, stride=2, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(3, 6, kernel_size=2, stride=2, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(6, 6, kernel_size=2, stride=2, padding=0),
-            nn.ReLU()
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(6 * 7 * 7, 20),
-            nn.ReLU(),
-            nn.Linear(20, num_classes)
-        )
+        pass
 
     def forward(self, x):
-        x = self.feature(x)
-        x = self.classifier(x)
-        
-        return x
+        pass
